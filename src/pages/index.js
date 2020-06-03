64eeca259890fb31bb48aa1a594546ad478c789a
Maghin <ahmedshabana0.0@gmail.com>
2020-06-03 02:16:56 +0200

    last changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,11 +15,11 @@ const IndexPage = () => (
       imgName="chechina"
       number="01"
       shoot="china"
-      title={'Creative Shoot'}
+      title={'Lantern Festival'}
       subtitle={'Photoshoot'}
       client={' Simple Simon Says'}
-      photographer={' Simple Simon Says'}
-      location={' Simple Simon Says'}
+      photographer={' @jia_chain'}
+      location={' Taichung, Taiwan'}
       paragraphone={
         ' Happy Lantern Festival 🎆🎆🎆 I did not have a chance to see the lanterns or put the sky lanterns today, but yesterday I rushed to Tainan Salt Water Bee Festival to be really cool! This experience is unforgettable!!! I will make another wave next year 😝💪'
       }
@@ -31,11 +31,11 @@ const IndexPage = () => (
       imgName="chemotor"
       number="02"
       shoot="motor"
-      title={'Creative Shoot'}
+      title={'Bike'}
       subtitle={'Photoshoot'}
       client={' Simple Simon Says'}
-      photographer={' Simple Simon Says'}
-      location={' Simple Simon Says'}
+      photographer={' @storyofyoumedia'}
+      location={' Orlando, Florida'}
       paragraphone={
         ' Do not get stuck doing certain things you do not enjoy doing. If you do not change your direction, you will never know where you would end up being. Life is limitless and full of possibilities. It is vital to believe in yourself that anything and everything you want can happen. In fact, everything already exist in the universe'
       }
--- src/pages/model.js
+++ src/pages/model.js
@@ -14,11 +14,11 @@ const Model = () => (
       imgName="chejungle"
       number="01"
       shoot="jungle"
-      title={'Jungle'}
-      subtitle={'Native theme Photoshoot'}
+      title={'Nature'}
+      subtitle={'Photoshoot'}
       client={' Che Yen '}
-      photographer={' Simple Simon Says'}
-      location={' Simple Simon Says'}
+      photographer={'@simplesimonsayss'}
+      location={' Jay Blanchard Park'}
       paragraphone={
         ' Happy Halloween! It is your Gaia here coming out from the dark trying to save her beloved planet 🌎🌿💕✨'
       }
@@ -30,11 +30,11 @@ const Model = () => (
       imgName="chemotor"
       number="02"
       shoot="motor"
-      title={'Creative Shoot'}
+      title={'Bike'}
       subtitle={'Photoshoot'}
       client={' Simple Simon Says'}
-      photographer={' Simple Simon Says'}
-      location={' Simple Simon Says'}
+      photographer={' @storyofyoumedia'}
+      location={' Orlando, Florida'}
       paragraphone={
         ' Do not get stuck doing certain things you do not enjoy doing. If you do not change your direction, you will never know where you would end up being. Life is limitless and full of possibilities. It is vital to believe in yourself that anything and everything you want can happen. In fact, everything already exist in the universe'
       }
@@ -46,11 +46,11 @@ const Model = () => (
       imgName="chepark"
       number="03"
       shoot="park"
-      title={'Creative Shoot'}
+      title={'Park Unicorn'}
       subtitle={'Photoshoot'}
       client={' Simple Simon Says'}
-      photographer={' Monica S.'}
-      location={' Simple Simon Says'}
+      photographer={' @themonkvisuals '}
+      location={' Tampa, Florida'}
       paragraphone={
         ' The struggles you go through in life can only strengthen you and make you grow. Look at your reflection and ask yourself: Are you happy where you are now? The answer is usually simple and clear. Do not wait for things to happen and do not ignore the little whispers. I am ready to let go and start a new chapter.. Thank you for the pics, Minh!'
       }
@@ -76,11 +76,11 @@ const Model = () => (
       imgName="chechina"
       number="05"
       shoot="china"
-      title={'Creative Shoot'}
+      title={'Lantern Festival'}
       subtitle={'Photoshoot'}
       client={' Simple Simon Says'}
-      photographer={' Simple Simon Says'}
-      location={' Simple Simon Says'}
+      photographer={' @jia_chain'}
+      location={' Taichung, Taiwan'}
       paragraphone={
         ' Happy Lantern Festival 🎆🎆🎆 I did not have a chance to see the lanterns or put the sky lanterns today, but yesterday I rushed to Tainan Salt Water Bee Festival to be really cool! This experience is unforgettable!!! I will make another wave next year 😝💪'
       }
@@ -92,11 +92,11 @@ const Model = () => (
       imgName="chelights"
       number="06"
       shoot="lights"
-      title={'Creative Shoot'}
+      title={'Christmas'}
       subtitle={'Photoshoot'}
       client={' Simple Simon Says'}
-      photographer={' Simple Simon Says'}
-      location={' Simple Simon Says'}
+      photographer={' @thefatmonkvisuals'}
+      location={' Orlando, Florida'}
       paragraphone={
         ' Love & Lights 💕✨🙏 Merry Christmas everyone 🎄🎅🏻🎁🎉 Swipe to see the other side of my face that you have never seen before in my pics lol'
       }
@@ -108,11 +108,11 @@ const Model = () => (
       imgName="cheorange"
       number="07"
       shoot="orange"
-      title={'Creative Shoot'}
+      title={'Lunar'}
       subtitle={'Photoshoot'}
-      client={' Simple Simon Says'}
-      photographer={' Simple Simon Says'}
-      location={' Simple Simon Says'}
+      client={' Che Yen'}
+      photographer={'@marcblairphoto'}
+      location={' Orlando, Florida'}
       paragraphone={
         ' Chinese New Year is a big deal in my country (its the biggest & most special festival for asians), but Ive completely forgotten how its like to celebrate this special day. Its been awhile since I celebrated Chinese New Year because I have no families here and no friends lol 😓'
       }
